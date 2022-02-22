66444ac7b7d5b34169883d524a8f8d94f78c1631
Terry Moreland <tsmoreland@live.com>
2022-02-22 00:08:26 +0000

    moved 'entities' / models to core library which has no dependencies

--- IssueTracker.Core/Model/Issue.cs
+++ IssueTracker.Core/Model/Issue.cs
@@ -11,9 +11,7 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-using System.Diagnostics.CodeAnalysis;
-
-namespace IssueTracker.App.Model;
+namespace IssueTracker.Core.Model;
 
 /// <summary>
 /// Issue Entity
