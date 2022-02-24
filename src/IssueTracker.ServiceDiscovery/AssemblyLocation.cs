361ef34ce4fbc32f24e5c5519c02eac45fd30ded
Terry Moreland <tsmoreland@live.com>
2022-02-24 21:18:57 +0000

    model class used to find the assemblies with HostingStartup's

---
+++ src/IssueTracker.ServiceDiscovery/AssemblyLocation.cs
@@ -0,0 +1,67 @@
+﻿// 
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System.Collections.Immutable;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+
+namespace IssueTracker.ServiceDiscovery;
+
+internal record struct AssemblyLocation(Assembly Assembly, string Folder, string Fullpath)
+{
+    public static AssemblyLocation? FromAssembly(Assembly? assembly)
+    {
+        if (assembly is null)
+        {
+            return null;
+        }
+
+        string? appFolder = Path.GetDirectoryName(assembly.Location);
+        return appFolder is null
+            ? null
+            : new AssemblyLocation(assembly, appFolder, assembly.Location);
+    }
+
+    public IEnumerable<AssemblyName> GetHostingStartupAssemblies(string rootNamespace)
+    {
+        ImmutableArray<AssemblyName> referencedAssemblyNames = Assembly.GetReferencedAssemblies()
+            .Where(asm => asm.FullName.StartsWith(rootNamespace))
+            .ToImmutableArray();
+
+        List<AssemblyName> assemblyNames = GetRelatedAssemblyFilenames(rootNamespace)
+            .Select(AssemblyName.GetAssemblyName)
+            .Where(asm => !referencedAssemblyNames.Contains(asm))
+            .ToList();
+        assemblyNames.ForEach(asm => Assembly.Load(asm));
+
+        return assemblyNames
+            .Select(Assembly.Load)
+            .Where(ContainsHostingStartup)
+            .Select(asm => asm.GetName());
+    }
+
+    public IEnumerable<AssemblyName> GetReferencedAssemblies(string rootNamespace)
+    {
+        return Assembly.GetReferencedAssemblies()
+            .Where(assemblyName => assemblyName.Name?.StartsWith(rootNamespace) == true);
+    }
+
+    private IEnumerable<string> GetRelatedAssemblyFilenames(string rootNamespace)
+    {
+        return Directory.GetFiles(Folder, $"{rootNamespace}.*.dll");
+    }
+
+    private static bool ContainsHostingStartup(Assembly assembly)
+    {
+        return assembly.GetTypes().Any(type => typeof(IHostingStartup).IsAssignableFrom(type));
+    }
+}
