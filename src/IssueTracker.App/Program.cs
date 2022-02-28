9861eec8c0a5e846bb8de6f944b53f191728d344
Terry Moreland <tsmoreland@live.com>
2022-02-28 19:49:46 +0000

    add xml serializers to program

--- src/IssueTracker.App/Program.cs
+++ src/IssueTracker.App/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddProblemDetails();
 
 builder.Services
     .AddControllers()
+    .AddXmlDataContractSerializerFormatters()
     .ConfigureApiBehaviorOptions(apiBehaviourOptions =>
     {
         apiBehaviourOptions.SuppressConsumesConstraintForFormFileParameters = true;
