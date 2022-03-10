2c9c15d5120c0149d8d0430e2d177407a02808f7
Terry Moreland <tsmoreland@live.com>
2022-03-10 00:16:27 +0000

    added new handlers for the new requests

---
+++ src/IssueTracker.Services/GetParentIssuesRequestHandler.cs
@@ -0,0 +1,52 @@
+﻿//
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System.Runtime.CompilerServices;
+using IssueTracker.Core.Projections;
+using IssueTracker.Data.Abstractions;
+using IssueTracker.Services.Abstractions.Model.Response;
+using IssueTracker.Services.Abstractions.Requests;
+using MediatR;
+
+namespace IssueTracker.Services;
+
+public sealed class GetParentIssuesRequestHandler : IRequestHandler<GetParentIssuesRequest, IAsyncEnumerable<IssueSummaryDto>> 
+{
+    private readonly IIssueRepository _repository;
+
+    public GetParentIssuesRequestHandler(IIssueRepository repository)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    /// <inheritdoc />
+    public Task<IAsyncEnumerable<IssueSummaryDto>> Handle(GetParentIssuesRequest request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(HandleWithEnumerable(request, cancellationToken));
+    }
+
+    private async IAsyncEnumerable<IssueSummaryDto> HandleWithEnumerable(GetParentIssuesRequest request,
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        (Guid id, int pageNumber, int pageSize) = request;
+
+        ConfiguredCancelableAsyncEnumerable<IssueSummaryProjection> issues = _repository
+            .GetParentIssues(id, pageNumber, pageSize, cancellationToken)
+            .WithCancellation(cancellationToken);
+
+        await foreach ((Guid issueId, string name) in issues)
+        {
+            yield return new IssueSummaryDto(issueId, name);
+        }
+    }
+}
