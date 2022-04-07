a4e47db052866374d65f4593581b0648f30ef89e
Terry Moreland <terry.s.moreland@gmail.com>
2022-04-07 14:26:51 +0100

    Domain models and eventual mediator classes

---
+++ src/IssueTracker.Core/Model/Issue.cs
@@ -0,0 +1,187 @@
+﻿//
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IssueTracker.Core.Model
+{
+
+    /// <summary>
+    /// Issue Entity
+    /// </summary>
+    public sealed class Issue
+    {
+        private IList<LinkedIssue> ParentIssueEntities { get; set; } = new List<LinkedIssue>();
+        private IList<LinkedIssue> ChildIssueEntities { get; set; } = new List<LinkedIssue>();
+
+        /// <summary>
+        /// Instanties a new instance of <see cref="Issue"/>
+        /// </summary>
+        /// <param name="id"></param>
+        public Issue(Guid id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// Instanties a new instance of <see cref="Issue"/>
+        /// </summary>
+        public Issue()
+            : this(Guid.NewGuid())
+        {
+        }
+
+        /// <summary>
+        /// Instanties a new instance of <see cref="Issue"/>
+        /// </summary>
+        public Issue(string title, string description, Priority priority, IssueType type)
+            : this(Guid.NewGuid())
+        {
+            Title = title;
+            Description = description;
+            Priority = priority;
+            Type = type;
+            LastUpdated = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Instanties a new instance of <see cref="Issue"/>, constructor provided primarily for use with entity framework
+        /// </summary>
+        public Issue(
+            Guid id,
+            string title,
+            string description,
+            Priority priority,
+            IssueType type,
+            DateTime lastUpdated,
+            string concurrencyToken,
+            IEnumerable<LinkedIssue> parentIssueEntities,
+            IEnumerable<LinkedIssue> childIssueEntities)
+            : this(id)
+        {
+            Title = title;
+            Description = description;
+            Priority = priority;
+            Type = type;
+            LastUpdated = lastUpdated;
+            ConcurrencyToken = concurrencyToken;
+            ParentIssueEntities = parentIssueEntities.ToList();
+            ChildIssueEntities = childIssueEntities.ToList();
+        }
+
+        public Guid Id { get; private set; } 
+
+        /// <summary>
+        /// Issue Title
+        /// </summary>
+        public string Title { get; private set; } = string.Empty;
+
+        /// <summary>
+        /// Issue Description
+        /// </summary>
+        public string Description { get; private set; } = string.Empty;
+
+        /// <summary>
+        /// Issue Priority
+        /// </summary>
+        public Priority Priority { get; private set; } = Priority.Low;
+
+        /// <summary>
+        /// Issue Type
+        /// </summary>
+        public IssueType Type { get; private set; } = IssueType.Defect;
+
+        /// <summary>
+        /// Last Updated field used for auditing
+        /// </summary>
+        public DateTime LastUpdated { get; private set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Concurrency token used to detected threading issues when persisting to the database
+        /// </summary>
+        public string ConcurrencyToken { get; private set; } = Guid.NewGuid().ToString();
+
+        /// <summary>
+        /// Issues that are linked to this one, this also serves as the ones that may be
+        /// blocking this issue
+        /// </summary>
+        public IEnumerable<Issue> ParentIssues => ParentIssueEntities.Select(i => i.ParentIssue);
+
+        /// <summary>
+        /// issues which this one links to, this also serves as issues that may be blocked by
+        /// this issue
+        /// </summary>
+        public IEnumerable<Issue> ChildIssues => ChildIssueEntities.Select(i => i.ChildIssue);
+
+        /// <summary>
+        /// adds link to child issue 
+        /// </summary>
+        /// <param name="linkType">the type of link</param>
+        /// <param name="issue">the issue to link to</param>
+        public void LinkToIssue(LinkType linkType, Issue issue)
+        {
+            ChildIssueEntities.Add(new LinkedIssue(linkType, this, issue));
+        }
+
+        /// <summary>
+        /// Update the issue name
+        /// </summary>
+        /// <param name="value">new name</param>
+        /// <exception cref="ArgumentException">if name is empty or whitespace</exception>
+        public void SetTitle(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Title cannot be empty");
+            }
+
+            Title = value;
+            LastUpdated = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Update issue description
+        /// </summary>
+        public void SetDescription(string value)
+        {
+            Description = !string.IsNullOrEmpty(value)
+                ? value.Trim()
+                : string.Empty;
+            LastUpdated = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Upates top level properties with values from <paramref name="source"/>.
+        /// Does not affect linked issues
+        /// </summary>
+        public void CopyTopLevelFrom(Issue source)
+        {
+            Title = source.Title;
+            Description = source.Description;
+            Priority = source.Priority;
+
+            LastUpdated = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Update issue priority
+        /// </summary>
+        public void ChangePriority(Priority priority)
+        {
+            Priority = priority;
+            LastUpdated = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/IssueTracker.Core/Projections/IssueSummaryProjection.cs
@@ -0,0 +1,70 @@
+﻿//
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+namespace IssueTracker.Core.Projections
+{
+    public sealed class IssueSummaryProjection : IEquatable<IssueSummaryProjection>
+    {
+        public IssueSummaryProjection(Guid id, string name)
+        {
+            Id = id;
+            Title = name ?? string.Empty;
+        }
+
+        public Guid Id { get; }
+        public string Title { get; }
+
+
+        public IssueSummaryProjection With(Guid? id = null, string name = null)
+        {
+            return new IssueSummaryProjection(id ?? Id, name ?? Title);
+        }
+
+        public void Deconstruct(out Guid id, out string name)
+        {
+            id = Id;
+            name = Title;
+        }
+
+        /// <inheritdoc />
+        public bool Equals(IssueSummaryProjection other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Id.Equals(other.Id) && Title == other.Title;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) =>
+            ReferenceEquals(this, obj) || obj is IssueSummaryProjection other && Equals(other);
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Id.GetHashCode() * 397) ^ (Title != null ? Title.GetHashCode() : 0);
+            }
+        }
+    }
+}
