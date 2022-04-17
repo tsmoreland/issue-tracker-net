e72cc29e8fd3fadf391557d6ddaf69b83be838bb
Terry Moreland <tsmoreland@live.com>
2022-04-17 13:03:53 +0100

    setting up services and middleware - moved kestrel config to code rather than config - added logging support - currently failing to run on Windows 11, possibly due to TLS 1.3 support which is apparently flakey

--- src/IssueTracker.GrpcApi.App/Program.cs
+++ src/IssueTracker.GrpcApi.App/Program.cs
@@ -1,16 +1,62 @@
+using System.IO.Compression;
+using Hellang.Middleware.ProblemDetails;
+using IssueTracker.Data.Abstractions;
 using IssueTracker.GrpcApi.Services;
+using IssueTracker.Middelware.SecurityHeaders;
+using IssueTracker.ServiceDiscovery;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.ResponseCompression;
+using Serilog;
+
+HostingStartupDiscovery
+    .DiscoverUnloadedAssembliesContainingHostingStartup()
+    .SetHostingStartupAssemblies();
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-ConfigureServices(builder.Services, builder.Environment);
+builder.Host
+    .UseSerilog((context, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(context.Configuration));
+builder.WebHost
+    .ConfigureKestrel(kestrelServerOptions =>
+    {
+        kestrelServerOptions.AddServerHeader = false;
+        kestrelServerOptions.ConfigureEndpointDefaults(endpointDefaults =>
+            endpointDefaults.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2);
+        kestrelServerOptions.ConfigureHttpsDefaults(httpsOptions =>
+            httpsOptions.SslProtocols =  System.Security.Authentication.SslProtocols.Tls11 | System.Security.Authentication.SslProtocols.Tls12);
+    });
+ConfigureServices(builder.Services, builder.Environment, builder.Configuration);
 
 WebApplication app = builder.Build();
+
+using (IServiceScope scope = app.Services.CreateScope())
+{
+    IIssueDataMigration migration =  scope.ServiceProvider.GetRequiredService<IIssueDataMigration>();
+    migration.Migrate();
+}
+
 ConfigurePipeline(app);
 
 app.Run();
 
-static void ConfigureServices(IServiceCollection services, IHostEnvironment environment)
+static void ConfigureServices(IServiceCollection services, IHostEnvironment environment, IConfiguration configuration)
 {
-    services.AddControllers();
+    services.AddProblemDetails();
+    services
+        .AddSecurityHeaders(configuration)
+        .AddResponseCompression(options =>
+        {
+            options.EnableForHttps = true;
+            options.Providers.Add<BrotliCompressionProvider>();
+            options.Providers.Add<GzipCompressionProvider>();
+        })
+        .Configure<BrotliCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal)
+        .AddHttpContextAccessor()
+        .Configure<DataProtectionTokenProviderOptions>(tokenProviderOptions =>
+        {
+            tokenProviderOptions.TokenLifespan = TimeSpan.FromHours(1);
+        })
+        .AddControllers();
+
     if (environment.IsDevelopment())
     {
         services.AddGrpc(options => options.EnableDetailedErrors = true);
@@ -19,17 +65,21 @@ static void ConfigureServices(IServiceCollection services, IHostEnvironment envi
     {
         services.AddGrpc();
     }
+
 }
 
 static void ConfigurePipeline(WebApplication app)
 {
+    //app.UseSecurityHeaders();
+    //app.UseProblemDetails();
+
     if (app.Environment.IsDevelopment())
     {
     }
     else
     {
-        app.UseHsts();
-        app.UseHttpsRedirection();
+        //app.UseHsts();
+        //app.UseHttpsRedirection();
     }
     //app.UseMigrationsEndPoint();
  
@@ -38,6 +88,11 @@ static void ConfigurePipeline(WebApplication app)
     app.UseAuthentication();
     app.UseAuthorization();
 
-    app.MapGrpcServices();
+    app
+        .UseEndpoints(endpoints =>
+        {
+            endpoints.MapGrpcServices();
+            endpoints.MapGet("/", () => new { error = "REST not supported, use GRPC client" });
+        });
 }
 
