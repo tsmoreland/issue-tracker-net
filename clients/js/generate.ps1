0b3734a68fc6dbd561393516f8248f9d77ed807a
Terry Moreland <tsmoreland@live.com>
2022-04-18 20:54:35 +0100

    remove client code, moved to another repo

--- clients/js/generate.ps1
+++
@@ -1,5 +0,0 @@
-Write-Output "`$env:Path = `$env:Path + './node_modules/.bin;./node_modules/grpc-tools/bin;./node_modules/protoc-gen-grpc-web/bin;'"
-protoc.exe -I ../.. `
-           --js_out=import_style=commonjs:./ `
-           --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./ `
-           issueTracker.proto
