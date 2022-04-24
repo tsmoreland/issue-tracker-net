4d641ad6dedfe79e28a470e2e5397730bbc67d24
Terry Moreland <tsmoreland@live.com>
2022-04-24 19:22:24 +0100

    netcoreapp2.1 implementation (in progress)

---
+++ server/net48-netcoreapp3.1/src/IssueTracker.Core/Views/LinkedIssueView.cs
@@ -0,0 +1,39 @@
+﻿//
+// Copyright © 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using IssueTracker.Core.Model;
+
+namespace IssueTracker.Core.Views;
+
+public sealed class LinkedIssueView
+{
+    private readonly Issue _issue;
+
+    public LinkedIssueView(LinkType linkType, Issue issue)
+    {
+        _issue = issue;
+        LinkType = linkType;
+    }
+
+    public int Id => _issue.Id;
+    public string Title => _issue.Title;
+    public string Description => _issue.Description;
+    public Priority Priority => _issue.Priority;
+    public IssueType Type => _issue.Type;
+    public LinkType LinkType { get; }
+    public DateTime LastUpdated => _issue.LastUpdated;
+    public IEnumerable<LinkedIssueView> Parents => _issue.ParentIssues;
+    public IEnumerable<LinkedIssueView> Children => _issue.ChildIssues;
+}
---
+++ server/net48-netcoreapp3.1/src/IssueTracker.EFCore21.Data/IssuesDbContext.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using IssueTracker.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
+
+namespace IssueTracker.EFCore21.Data;
+
+public sealed class IssuesDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    /// <summary>
+    /// Instantiates a new instance of the <see cref="IssuesDbContext"/> class.
+    /// </summary>
+    public IssuesDbContext(DbContextOptions<IssuesDbContext> dbContextOptions, IConfiguration configuration)
+        : base(dbContextOptions)
+    {
+        _configuration = configuration;
+    }
+
+    /// <summary>
+    /// Issues data set
+    /// </summary>
+    public DbSet<Issue> Issues { get; private set; } = null!;
+
+    /// <summary>
+    /// Linked Issues data set
+    /// </summary>
+    public DbSet<LinkedIssue> LinkedIssues { get; private set; } = null!;
+
+    /// <inheritdoc />
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured)
+        {
+            return;
+        }
+
+        string connectionString = _configuration.GetConnectionString("ApplicationConnection");
+        optionsBuilder
+            .UseSqlite(
+                connectionString,
+                options => options.MigrationsAssembly(typeof(IssuesDbContext).Assembly.FullName));
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        EntityTypeBuilder<Issue> issueEntity = modelBuilder.Entity<Issue>();
+        issueEntity.ToTable("Issues").HasKey(e => e.Id);
+        issueEntity.HasIndex(e => e.Title);
+        issueEntity.HasIndex(e => e.Priority);
+
+        issueEntity.Property(e => e.Id).ValueGeneratedOnAdd().IsRequired();
+        issueEntity.Property(e => e.Title).IsRequired().IsUnicode().HasMaxLength(200);
+        issueEntity.Property(e => e.Description).IsUnicode().HasMaxLength(500);
+        issueEntity.Property(e => e.Priority).IsRequired();
+        issueEntity.Property(e => e.ConcurrencyToken).IsConcurrencyToken();
+        issueEntity.HasMany("ChildIssueEntities").WithOne();
+        issueEntity.HasMany("ParentIssueEntities").WithOne();
+
+        // without these we'd get foregin key warnings becuase we're using ParentIssueEntities and ChildIssueEntites as the data backing (private properties)
+        issueEntity.Ignore(e => e.ParentIssues);
+        issueEntity.Ignore(e => e.ChildIssues);
+
+        Seed(modelBuilder);
+    }
+
+    private static void Seed(ModelBuilder modelBuilder)
+    {
+        EntityTypeBuilder<Issue> issueEntity = modelBuilder.Entity<Issue>();
+
+        Issue first = new(1, "First", "First issue",
+            Priority.Medium, IssueType.Epic,
+            new DateTime(2022, 01, 01, 0, 0, 0, DateTimeKind.Utc), Guid.NewGuid().ToString(),
+            Array.Empty<LinkedIssue>(), Array.Empty<LinkedIssue>());
+        Issue second = new (2, "Second", "Second issue",
+            Priority.Low, IssueType.Story,
+            new DateTime(2022, 01, 020, 0, 0, 0, DateTimeKind.Utc), Guid.NewGuid().ToString(),
+            Array.Empty<LinkedIssue>(), Array.Empty<LinkedIssue>());
+        Issue third = new (3, "Third", "Third issue",
+            Priority.Medium, IssueType.Story,
+            new DateTime(2022, 01, 020, 0, 0, 0, DateTimeKind.Utc), Guid.NewGuid().ToString(),
+            Array.Empty<LinkedIssue>(), Array.Empty<LinkedIssue>());
+        issueEntity.HasData(first, second, third);
+
+        EntityTypeBuilder<LinkedIssue> linkedIssuesEntity = modelBuilder.Entity<LinkedIssue>();
+        linkedIssuesEntity.ToTable("LinkedIssue")
+            .HasKey(e => new { e.ParentIssueId, e.ChildIssueId });
+        linkedIssuesEntity
+            .HasOne(e => e.ParentIssue)
+            .WithMany("ParentIssueEntities")
+            .HasPrincipalKey(e => e.Id)
+            .HasForeignKey(e => e.ParentIssueId)
+            .OnDelete(DeleteBehavior.Restrict);
+        linkedIssuesEntity
+            .HasOne(e => e.ChildIssue)
+            .WithMany("ChildIssueEntities")
+            .HasPrincipalKey(e => e.Id)
+            .HasForeignKey(e => e.ChildIssueId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        LinkedIssue secondToThird = new(LinkType.Related, 2, 3);
+        linkedIssuesEntity.HasData(secondToThird);
+    }
+
+
+}
