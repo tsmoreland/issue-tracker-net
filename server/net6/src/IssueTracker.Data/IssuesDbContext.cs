b8060bdb58f275637fa31644629e38c495a4ad0c
Terry Moreland <terry.s.moreland@gmail.com>
2022-04-24 13:33:20 +0100

    add owned property Reporter/Assignee, removed seeded data from HasData

--- server/net6/src/IssueTracker.Data/IssuesDbContext.cs
+++ server/net6/src/IssueTracker.Data/IssuesDbContext.cs
@@ -12,6 +12,7 @@
 //
 
 using IssueTracker.Core.Model;
+using IssueTracker.Core.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.Extensions.Configuration;
@@ -74,6 +75,41 @@ public sealed class IssuesDbContext : DbContext
         issueEntity.Property(e => e.Description).IsUnicode().HasMaxLength(500);
         issueEntity.Property(e => e.Priority).IsRequired();
         issueEntity.Property(e => e.ConcurrencyToken).IsConcurrencyToken();
+
+        issueEntity.OwnsOne(e => e.Assignee,
+            (owned) =>
+            {
+                owned
+                    .Property(e => e.Id)
+                    .IsRequired()
+                    .HasDefaultValue(User.Unassigned.Id)
+                    .HasColumnName("AssigneeId");
+                owned
+                    .Property(e => e.FullName)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(true)
+                    .HasDefaultValue(User.Unassigned.FullName)
+                    .HasColumnName("AssigneeFullName");
+            });
+        issueEntity.OwnsOne(e => e.Reporter,
+            (owned) =>
+            {
+                owned
+                    .Property(e => e.Id)
+                    .IsRequired()
+                    .HasDefaultValue(User.Unassigned.Id)
+                    .HasColumnName("ReporterId");
+                owned
+                    .Property(e => e.FullName)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(true)
+                    .HasDefaultValue(User.Unassigned.FullName)
+                    .HasColumnName("ReporterFullName");
+            });
+
+
         issueEntity.HasMany("ChildIssueEntities").WithOne();
         issueEntity.HasMany("ParentIssueEntities").WithOne();
 
@@ -81,38 +117,24 @@ public sealed class IssuesDbContext : DbContext
         issueEntity.Ignore(e => e.ParentIssues);
         issueEntity.Ignore(e => e.ChildIssues);
 
-        Issue first = new(new Guid("1385056E-8AFA-4E09-96DF-AE12EFDF1A29"), "First", "First issue",
-            Priority.Medium, IssueType.Epic,
-            new DateTime(2022, 01, 01, 0, 0, 0, DateTimeKind.Utc), Guid.NewGuid().ToString(),
-            Array.Empty<LinkedIssue>(), Array.Empty<LinkedIssue>());
-        Issue second = new (new Guid("A28B8C45-6668-4169-940C-C16D71EB69DE"), "Second", "Second issue",
-            Priority.Low, IssueType.Story,
-            new DateTime(2022, 01, 020, 0, 0, 0, DateTimeKind.Utc), Guid.NewGuid().ToString(),
-            Array.Empty<LinkedIssue>(), Array.Empty<LinkedIssue>());
-        Issue third = new (new Guid("502AD68E-7B37-4426-B422-23B6A9B1B7CA"), "Third", "Third issue",
-            Priority.Medium, IssueType.Story,
-            new DateTime(2022, 01, 020, 0, 0, 0, DateTimeKind.Utc), Guid.NewGuid().ToString(),
-            Array.Empty<LinkedIssue>(), Array.Empty<LinkedIssue>());
-        issueEntity.HasData(first, second, third);
+        EntityTypeBuilder<LinkedIssue> linkedIssueEntity = modelBuilder.Entity<LinkedIssue>();
+        linkedIssueEntity.ToTable("LinkedIssues").HasKey(e => e.Id);
+        linkedIssueEntity.Property(e => e.Id).ValueGeneratedOnAdd().IsRequired();
+        linkedIssueEntity.HasIndex(e => e.ParentIssueId);
+        linkedIssueEntity.HasIndex(e => e.ChildIssueId);
+        linkedIssueEntity.HasOne(e => e.ParentIssue).WithMany("ParentIssueEntities").HasForeignKey(e => e.ParentIssueId);
+        linkedIssueEntity.HasOne(e => e.ChildIssue).WithMany("ChildIssueEntities").HasForeignKey(e => e.ChildIssueId);
 
-        EntityTypeBuilder<LinkedIssue> linkedIssuesEntity = modelBuilder.Entity<LinkedIssue>();
-        linkedIssuesEntity.ToTable("LinkedIssue")
-            .HasKey(e => new { e.ParentIssueId, e.ChildIssueId });
-        linkedIssuesEntity
-            .HasOne(e => e.ParentIssue)
-            .WithMany("ParentIssueEntities")
-            .HasPrincipalKey(e => e.Id)
-            .HasForeignKey(e => e.ParentIssueId)
-            .OnDelete(DeleteBehavior.Restrict);
-        linkedIssuesEntity
-            .HasOne(e => e.ChildIssue)
-            .WithMany("ChildIssueEntities")
-            .HasPrincipalKey(e => e.Id)
-            .HasForeignKey(e => e.ChildIssueId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        LinkedIssue secondToThird = new(LinkType.Related, new Guid("A28B8C45-6668-4169-940C-C16D71EB69DE"), new Guid("502AD68E-7B37-4426-B422-23B6A9B1B7CA"));
-        linkedIssuesEntity.HasData(secondToThird);
-
+        // HasData to seed is not done here due to complexities of handling Owned Types (at time of writing Assignee and Reporter)
+        // in order to use HasData to see these values it would need to be done as part of the OwnsOne:
+        /*
+        issueEntity.OwnsOne(e => e.Reporter)
+            .HasData(new
+            {
+                UserId = "id of the issue entity", // this is the shadow property created by EF to link the objects, User matches the class name
+                Id = "id property of the user object",
+                FullName = "fullname property from user table"
+            });
+        */
     }
 }
