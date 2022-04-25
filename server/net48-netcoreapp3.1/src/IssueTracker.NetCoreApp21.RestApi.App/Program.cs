06314da9435468598ce7d3b84f4e581389438a98
Terry Moreland <tsmoreland@live.com>
2022-04-26 00:28:33 +0100

    disable server header

--- server/net48-netcoreapp3.1/src/IssueTracker.NetCoreApp21.RestApi.App/Program.cs
+++ server/net48-netcoreapp3.1/src/IssueTracker.NetCoreApp21.RestApi.App/Program.cs
@@ -22,5 +22,6 @@ public class Program
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
+            .UseKestrel(options => options.AddServerHeader = false)
             .UseStartup<Startup>();
 }
