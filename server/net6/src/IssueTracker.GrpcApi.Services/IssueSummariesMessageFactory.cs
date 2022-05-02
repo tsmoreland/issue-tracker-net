e2b96be152920726a269a4f1b4277f19e787f128
Terry Moreland <tsmoreland@live.com>
2022-05-02 02:32:45 +0100

    add issue type to summary

--- server/net6/src/IssueTracker.GrpcApi.Services/IssueSummariesMessageFactory.cs
+++ server/net6/src/IssueTracker.GrpcApi.Services/IssueSummariesMessageFactory.cs
@@ -51,6 +51,7 @@ internal static class IssueSummariesMessageFactory
         {
             Id = projection.Id.ToString(),
             Title = projection.Title,
+            Type = (int)projection.Type,
         };
     }
 }
