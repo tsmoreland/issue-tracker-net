58fdabd88a1c01625d7bb68c9a0b8d5cc2262ef7
Terry Moreland <tsmoreland@live.com>
2022-05-11 01:20:06 +0100

    simplify cli use of services

--- server/net6/src/Issues/IssuesTracker.Issues.cli/Program.cs
+++ server/net6/src/Issues/IssuesTracker.Issues.cli/Program.cs
@@ -1,50 +1,24 @@
 ﻿using IssueTracker.Issues.Infrastructure;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Hosting.Internal;
-using Microsoft.Extensions.Logging;
 
-IConfiguration configuration = new ConfigurationBuilder()
-    .AddEnvironmentVariables()
-    .AddJsonFile("appsettings.json")
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(ConfigureServices)
     .Build();
 
-IServiceProvider provider = ConfigureServices(new ServiceCollection(), configuration)
-    .BuildServiceProvider();
 
-using IServiceScope scope = provider.CreateScope();
+using IServiceScope scope = host.Services.CreateScope();
 
 IssuesDbContext dbContext = scope.ServiceProvider.GetRequiredService<IssuesDbContext>();
 
 await dbContext.Database.MigrateAsync();
 
 
-static IServiceCollection ConfigureServices(IServiceCollection services, IConfiguration configuration)
+static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
 {
     services
-        .AddSingleton<ILoggerFactory>(new LoggerFactory())
-        .AddSingleton<IHostEnvironment>(BuildHostEnvironment())
-        .AddSingleton<IConfiguration>(configuration)
         .AddDbContext<IssuesDbContext>();
 
-
-    return services;
 }
 
-static IHostEnvironment BuildHostEnvironment()
-{
-    string root = Path.GetDirectoryName(typeof(Program).Assembly.Location) ?? Directory.GetCurrentDirectory();
-
-    HostingEnvironment hostingEnvironment = new()
-    {
-        ApplicationName = typeof(Program).Assembly.GetName().Name,
-        EnvironmentName = Environments.Production,
-        ContentRootPath = root,
-    };
-
-    hostingEnvironment.ContentRootFileProvider = new PhysicalFileProvider(root);
-    return hostingEnvironment;
-}
