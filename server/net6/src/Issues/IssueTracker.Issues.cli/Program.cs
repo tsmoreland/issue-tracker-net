16a54d0b391dee2b00f3cb1e059b5484722466a7
Terry Moreland <tsmoreland@live.com>
2022-05-15 22:12:44 +0100

    updated cli app to test out repository and api (mediator) v1, v1 is failing at present

--- server/net6/src/Issues/IssueTracker.Issues.cli/Program.cs
+++ server/net6/src/Issues/IssueTracker.Issues.cli/Program.cs
@@ -1,52 +1,140 @@
 ﻿using IssueTracker.Issues.Domain.ModelAggregates.IssueAggregate;
 using IssueTracker.Issues.Infrastructure;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(ConfigureServices)
-    .Build();
+using Version1 = IssueTracker.Issues.API.Version1.Abstractions;
+using Version2 = IssueTracker.Issues.API.Version2.Abstractions;
 
+
+#if ASPNETCORE
+using IssueTracker.ServiceDiscovery;
+// we're not but it shows  what it would look like if we were
+HostingStartupDiscovery
+    .DiscoverUnloadedAssembliesContainingHostingStartup()
+    .SetHostingStartupAssemblies();
+
+#endif
+
+
+IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args)
+    .UseSerilog((context, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(context.Configuration));
+
+#if !ASPNETCORE
+hostBuilder
+    .ConfigureServices(services =>
+    {
+        HostingStartup.Configure(services);
+        IssueTracker.Issues.API.HostingStartup.Configure(services);
+    });
+#endif
+
+IHost host = hostBuilder.Build();
+
+await InitializeDatabase(host.Services);
 
 using IServiceScope scope = host.Services.CreateScope();
-
-ILoggerFactory loggerFactory = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
-IssuesDbContext dbContext = scope.ServiceProvider.GetRequiredService<IssuesDbContext>();
-ILogger logger = loggerFactory.CreateLogger<IssuesDbContext>();
-
-await dbContext.Database.EnsureDeletedAsync();
-await dbContext.Database.MigrateAsync();
-
-Issue epic = new("APP",1, "Epic1", "...") { Type = IssueType.Epic };
-
-try
+if (args.Any())
 {
-    dbContext.Issues.Add(epic);
-    dbContext.SaveChanges();
-}
-catch (Exception ex)
-{
-    logger.LogError("{Exception}", ex.ToString());
-}
-
-Issue story = new("APP",2, "Story1", "...") { Type = IssueType.Story, EpicId = epic.Id };
-try
-{
-    dbContext.Issues.Add(story);
-    dbContext.SaveChanges();
-}
-catch (Exception ex)
-{
-    logger.LogError("{Exception}", ex.ToString());
+    switch (args[0].ToLowerInvariant())
+    {
+        case "dbcontext":
+            VerifyAddToContext(scope);
+            break;
+        case "repository":
+            await VerifyRepository(scope);
+            break;
+        case "apiv1":
+            await VerifyApiV1(scope);
+            break;
+        case "apiv2":
+            break;
+    }
 }
 
 
-static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
-{
-    services
-        .AddDbContext<IssuesDbContext>();
+await VerifyApiV1(scope);
 
+
+static async Task InitializeDatabase(IServiceProvider provider)
+{
+    using IServiceScope scope = provider.CreateScope();
+    IssuesDbContext dbContext = scope.ServiceProvider.GetRequiredService<IssuesDbContext>();
+
+    await dbContext.Database.EnsureDeletedAsync();
+    await dbContext.Database.MigrateAsync();
 }
 
+static void VerifyAddToContext(IServiceScope scope)
+{
+    IssuesDbContext dbContext = scope.ServiceProvider.GetRequiredService<IssuesDbContext>();
+    ILogger logger = scope.ServiceProvider
+        .GetRequiredService<ILoggerFactory>()
+        .CreateLogger<Program>();
+
+    Issue epic = new("APP",1, "Epic1", "...") { Type = IssueType.Epic };
+    try
+    {
+        dbContext.Issues.Add(epic);
+        dbContext.SaveChanges();
+    }
+    catch (Exception ex)
+    {
+        logger.LogError("{Exception}", ex.ToString());
+    }
+
+    Issue story = new("APP",2, "Story1", "...") { Type = IssueType.Story, EpicId = epic.Id };
+    try
+    {
+        dbContext.Issues.Add(story);
+        dbContext.SaveChanges();
+    }
+    catch (Exception ex)
+    {
+        logger.LogError("{Exception}", ex.ToString());
+    }
+}
+
+static async Task VerifyRepository(IServiceScope scope)
+{
+    IIssueRepository repository = scope.ServiceProvider.GetRequiredService<IIssueRepository>();
+    Issue epic = new("APP",1, "Epic1", "...") { Type = IssueType.Epic };
+
+    epic = repository.Add(epic);
+
+    Issue story = new("APP",2, "Story1", "...") { Type = IssueType.Story, EpicId = epic.Id };
+    story = repository.Add(story);
+
+    await repository.UnitOfWork.SaveEntitiesAsync();
+
+    DisplayIssue(epic);
+    DisplayIssue(story);
+}
+
+static async Task VerifyApiV1(IServiceScope scope)
+{
+    IMediator mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+    Version1.DataTransferObjects.IssueDto epic =
+        await mediator.Send(new Version1.Commands.CreateIssueCommand("app", "sample epic", "first epic", Priority.High));
+
+    Version1.DataTransferObjects.IssueDto story =
+        await mediator.Send(new Version1.Commands.CreateIssueCommand("app", "sample story", "first story", Priority.Medium));
+
+    DisplayIssueDto(epic);
+    DisplayIssueDto(story);
+}
+
+static void DisplayIssue(Issue issue)
+{
+    Console.WriteLine($"{issue.Id} {issue.Title}");
+}
+static void DisplayIssueDto(Version1.DataTransferObjects.IssueDto issue)
+{
+    Console.WriteLine($"{issue.Id} {issue.Title}");
+}
