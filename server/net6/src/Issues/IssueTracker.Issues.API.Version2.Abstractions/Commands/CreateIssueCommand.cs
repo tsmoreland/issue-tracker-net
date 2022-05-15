b3e3cb7c62bc42deb1794989732c679a4fb931a4
Terry Moreland <tsmoreland@live.com>
2022-05-15 12:54:45 +0100

    version 2 initial api abstractions

---
+++ server/net6/src/Issues/IssueTracker.Issues.API.Version2.Abstractions/Commands/CreateIssueCommand.cs
@@ -0,0 +1,13 @@
+﻿using IssueTracker.Issues.API.Version2.Abstractions.DataTransferObjects;
+using IssueTracker.Issues.Domain.ModelAggregates.IssueAggregate;
+using MediatR;
+
+namespace IssueTracker.Issues.API.Version2.Abstractions.Commands;
+
+public sealed record class CreateIssueCommand(
+    string Project,
+    string Title,
+    string Description,
+    Priority Priority,
+    IssueType Type,
+    IssueIdentifier? EpicId) : IRequest<IssueDto>;
