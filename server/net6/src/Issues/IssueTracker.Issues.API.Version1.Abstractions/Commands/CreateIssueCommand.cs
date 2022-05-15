825b723a0ce94d7fae64c584c0d830e85e6b6c3c
Terry Moreland <tsmoreland@live.com>
2022-05-15 12:54:34 +0100

    version 1 initial abstractions for api

---
+++ server/net6/src/Issues/IssueTracker.Issues.API.Version1.Abstractions/Commands/CreateIssueCommand.cs
@@ -0,0 +1,11 @@
+﻿using IssueTracker.Issues.API.Version1.Abstractions.DataTransferObjects;
+using IssueTracker.Issues.Domain.ModelAggregates.IssueAggregate;
+using MediatR;
+
+namespace IssueTracker.Issues.API.Version1.Abstractions.Commands;
+
+public sealed record class CreateIssueCommand(
+    string Project,
+    string Title,
+    string Description,
+    Priority Priority) : IRequest<IssueDto>;
