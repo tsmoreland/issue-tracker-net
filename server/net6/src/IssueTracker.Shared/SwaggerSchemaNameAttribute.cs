0c768bbaa37f72b3b20b43fb27ed25624e2d768d
Terry Moreland <tsmoreland@live.com>
2022-05-30 00:53:48 +0100

    moved swashbuckle abstractions over to shared

--- server/net6/src/IssueTracker.Shared/SwaggerSchemaNameAttribute.cs
+++ server/net6/src/IssueTracker.Shared/SwaggerSchemaNameAttribute.cs
@@ -11,7 +11,7 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-namespace IssueTracker.SwashbuckleExtensions.Abstractions;
+namespace IssueTracker.Shared;
 
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
 public sealed class SwaggerSchemaNameAttribute : Attribute
