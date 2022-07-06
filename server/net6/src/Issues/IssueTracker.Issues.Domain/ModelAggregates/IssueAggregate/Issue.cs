d6298d1d03b7f05d9d3492429420ac6c465133f7
Terry Moreland <tsmoreland@live.com>
2022-07-06 22:34:46 +0100

    remove unused using

--- server/net6/src/Issues/IssueTracker.Issues.Domain/ModelAggregates/IssueAggregate/Issue.cs
+++ server/net6/src/Issues/IssueTracker.Issues.Domain/ModelAggregates/IssueAggregate/Issue.cs
@@ -11,7 +11,6 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-using System.Diagnostics.CodeAnalysis;
 using IssueTracker.Issues.Domain.DataContracts;
 using IssueTracker.Issues.Domain.ModelAggregates.IssueAggregate.Commands;
 using IssueTracker.Issues.Domain.ModelAggregates.IssueAggregate.State;
