82e1bac3be57fdaa6290595b71a3e94fce244ef7
Terry Moreland <terry.s.moreland@gmail.com>
2022-09-30 11:55:45 +0100

    resolve some minor build failures (missing usings)

--- server/net48-netcoreapp3.1/src/IssueTracker.EFCore21.Data/ServiceCollectionExtensions.cs
+++ server/net48-netcoreapp3.1/src/IssueTracker.EFCore21.Data/ServiceCollectionExtensions.cs
@@ -11,9 +11,8 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-using IssueTracker.Core;
 using IssueTracker.Data.Abstractions;
-using Microsoft.EntityFrameworkCore;
+using IssueTracker.Shared;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
--- server/net48-netcoreapp3.1/src/IssueTracker.Services/ServiceCollectionExtensions.cs
+++ server/net48-netcoreapp3.1/src/IssueTracker.Services/ServiceCollectionExtensions.cs
@@ -11,8 +11,8 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-using IssueTracker.Core;
 using IssueTracker.Services.Handlers;
+using IssueTracker.Shared;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
