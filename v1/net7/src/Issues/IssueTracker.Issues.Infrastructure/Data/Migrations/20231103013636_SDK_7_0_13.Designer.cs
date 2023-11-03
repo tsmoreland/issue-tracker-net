299d9a727dfb2170f18072c60c8c841f0818601e
Terry Moreland <tsmoreland@live.com>
2023-11-03 01:37:11 +0000

    update database with latest sdk version

--- v1/net7/src/Issues/IssueTracker.Issues.Infrastructure/Data/Migrations/20231103013636_SDK_7_0_13.Designer.cs
+++ v1/net7/src/Issues/IssueTracker.Issues.Infrastructure/Data/Migrations/20231103013636_SDK_7_0_13.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IssueTracker.Issues.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IssueTracker.Issues.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(IssuesDbContext))]
-    partial class IssuesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231103013636_SDK_7_0_13")]
+    partial class SDK_7_0_13
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
---
+++ v1/net7/src/Issues/IssueTracker.Issues.Infrastructure/Data/Migrations/20231103013636_SDK_7_0_13.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IssueTracker.Issues.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SDK_7_0_13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
